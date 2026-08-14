8dcff2017e1e94e9c0c887bc2a959ec862196e9d
Steven <stevenlu@student.unimelb.edu.au>
2026-08-14 23:59:19 +1000

    fixed imports

--- scripts/ai/ai_agent.py
+++ scripts/ai/ai_agent.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
-from langchain.agents import create_tool_calling_agent, AgentExecutor
+from langchain_classic.agents import create_tool_calling_agent, AgentExecutor
 from scripts.ai.tools import search_tool, wiki_tool
 import os
 import scripts.ai.stt_tts
